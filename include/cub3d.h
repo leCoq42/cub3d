c538d1caf877e030d165612e9655300a3047f895
Martin Haan <mhaan@f1r5s9.codam.nl>
2023-10-23 17:04:29 +0200

    testing

--- include/cub3d.h
+++ include/cub3d.h
@@ -12,4 +12,50 @@
 #include "ft_printf.h"
 #include "get_next_line_bonus.h"
 
+#define screenWidth 1080
+#define screenHeight 720
+
+typedef union color
+{
+	uint32_t	c;
+	struct s_rgba
+	{
+		uint8_t	a;
+		uint8_t	b;
+		uint8_t	g;
+		uint8_t	r;
+	}	t_rgba;
+}	t_color;
+
+typedef struct s_point
+{
+	int32_t	x;
+	int32_t	y;
+	t_color	c;
+	char	type;
+}	t_point;
+
+typedef struct s_map
+{
+	int32_t	width;
+	int32_t	height;
+	t_point	**grid;
+}	t_map;
+
+typedef struct s_player
+{
+	double	x_pos;
+	double	y_pos;
+}	t_player;
+
+typedef struct s_cub3d
+{
+	t_map		*map;
+	t_player	player;
+	mlx_t		*mlx;
+	mlx_image_t	*img;
+	double		time;
+	double		oldtime;
+}	t_cub3d;
+
 #endif
