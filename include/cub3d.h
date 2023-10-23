f59048b0553dccea43e9f907f63c3c2f0af519b8
Martin Haan <mhaan@f1r5s9.codam.nl>
2023-10-23 11:40:19 +0200

    addd cub3d header

---
+++ include/cub3d.h
@@ -0,0 +1,15 @@
+#ifndef CUB3D_H
+# define CUB3D_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <limits.h>
+#include <math.h>
+
+#include "MLX42.h"
+#include "libft.h"
+#include "ft_printf.h"
+#include "get_next_line_bonus.h"
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include "cub3d.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+		return (1);
+
+}
