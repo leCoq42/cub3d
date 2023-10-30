bf13fcebe071809ceed7a16019f35186e466c28e
Victor Brouwer <vbrouwer@f1r6s8.codam.nl>
2023-10-30 14:35:36 +0100

    pre merge commits

--- src/main.c
+++ src/main.c
@@ -1,19 +1,6 @@
 #include "cub3d.h"
 #include "MLX42.h"
 
-// int	main(int argc, char **argv)
-// {
-// 	t_cub3d	*cub3d;
-
-// 	if (argc != 2)
-// 		exit(0);
-// 	cub3d = parse_file(argv[1]);
-// 	// mlx_start(map);
-// 	// clean_up_arr(map->points, map);
-// 	free(cub3d);
-// 	return (0);
-// }
-
 int main(int argc, char **argv)
 {
 	t_cub3d	*cub3d;
